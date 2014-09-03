7d70b9446d15e26cdf0fadc363d6915c94e2991a
Yang Lei <i@yangl1996.com>
2014-09-04 00:39:24 +0800

    Initial

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm776353091-03c4431c6f0b92d9732c3971','m776352715-f59b385fc00684c5a4b73d6f','m776359676-5e0337ed69528ebab2ceb20c', 
+];
+
+// refresh interval (in seconds)
+var __refresh = 900;
+
+// the default language
+// set false to disable language support and show only english,
+// or set null to use visitor's browser language
+var __language = null;

99bc57265b99306f90363c238704d930ad7b64af
Lei Yang <yltt1234512@gmail.com>
2016-05-18 14:46:25 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'm776353091-03c4431c6f0b92d9732c3971','m776352715-f59b385fc00684c5a4b73d6f','m776359676-5e0337ed69528ebab2ceb20c', 'm776951408-bc0c93099ecb2c4bf7c493c5', 'm776698091-5be29ebc997adacb6a39e1ff', 
+	'm776352715-f59b385fc00684c5a4b73d6f','m776359676-5e0337ed69528ebab2ceb20c', 'm776951408-bc0c93099ecb2c4bf7c493c5', 'm776698091-5be29ebc997adacb6a39e1ff', 
 ];
 
 // refresh interval (in seconds)
